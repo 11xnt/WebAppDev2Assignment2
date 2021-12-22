e6cd2929bedb920b6109b6eb0c3bcbf5f41bbe16
Allen Terescenco <allenterescencobq@gmail.com>
2021-12-22 21:29:51 +0000

    Added user route with authentication

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import './seedData'
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
-
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
     console.info(`Server running at ${port}`);
