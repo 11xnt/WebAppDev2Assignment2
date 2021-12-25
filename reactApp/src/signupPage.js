d6d681bac846ae228f88f17d0e5af5af57e4c333
Allen Terescenco <allenterescencobq@gmail.com>
2021-12-25 23:23:20 +0000

    Added signup page and works

--- reactApp/src/signupPage.js
+++ reactApp/src/signupPage.js
@@ -5,11 +5,13 @@ import { AuthContext } from './authContext';
 const SignUpPage = props => {
     const context = useContext(AuthContext)
     const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordAgain, setPasswordAgain] = useState("");
     const [registered, setRegistered] = useState(false);
 
     const register = () => {
-        if (userName.length > 3) {
-            context.register(userName);
+        if (password.length > 0 && password === passwordAgain) {
+            context.register(userName, password);
             setRegistered(true);
         }
     }
@@ -26,6 +28,12 @@ const SignUpPage = props => {
             <p>You must register a username and password to log in </p>
             <input value={userName} placeholder="user name" onChange={e => {
                 setUserName(e.target.value);
+            }}></input><br />
+            <input value={password} type="password" placeholder="password" onChange={e => {
+                setPassword(e.target.value);
+            }}></input><br />
+            <input value={passwordAgain} type="password" placeholder="password again" onChange={e => {
+                setPasswordAgain(e.target.value);
             }}></input><br />
             {/* Login web form  */}
             <button onClick={register}>Register</button>
